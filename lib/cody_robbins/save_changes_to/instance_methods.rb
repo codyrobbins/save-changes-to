7962d39b1608a26497fa5764189b2ec5e278cb26
Cody Robbins <cody@sakuzaku.com>
2011-05-24 19:20:56 -0400

    Added the documentation.

--- lib/cody_robbins/save_changes_to/instance_methods.rb
+++ lib/cody_robbins/save_changes_to/instance_methods.rb
@@ -1,26 +1,26 @@
 module CodyRobbins
   module CreateNew
     module InstanceMethods
-      # Creates a new instance of an ActiveRecord model and assigns it to an instance variable.
+      # Updates the attributes of an instance of a model in an appropriately named instance variable in the controller with the attributes in `params` corresponding to the model name.
       #
-      # * You pass the name of the model to create to the method.
+      # * You pass the name of the model to update to the method.
       # * The key in `params` assumed to contain the attributes of the model is extrapolated from the model name. For example, if the model is `User` then this key would be `:user`.
-      # * The instance variable that the new instance is assigned to will be named according to the model. For example, if the model is `User` then the instance variable will be `@user`.
+      # * The instance variable that is updated is assumed to be named according to the model. For example, if the model is `User` then the instance variable will be `@user`.
       #
-      # @param name [Symbol, String] The name of the model to create.
-      # @return Returns the new instance of the model.
+      # @param name [Symbol, String] The name of the model to update.
+      # @return [boolean] Whether the update was successful.
       #
       # @example The following two actions are equivalent.
       #
       #     class UserController < ApplicationController
       #       # The usual Rails way.
-      #       def create
-      #         @user = User.new(params[:user])
+      #       def update
+      #         @user.update_attributes(params[:user])
       #       end
       #
-      #       # Using create-new.
-      #       def create
-      #         create_new(:user)
+      #       # Using save-changes-to.
+      #       def update
+      #         save_changes_to(:user)
       #       end
       #     end
       def save_changes_to(name)
